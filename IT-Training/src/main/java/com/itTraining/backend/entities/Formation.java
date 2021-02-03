9d51197953e8c3bf48904b97a36c12a6a7ee5af1
Pierre Krepa <pierre.krepa@hotmail.com>
2021-02-03 17:01:06 +0100

    Entité formations

--- IT-Training/src/main/java/com/itTraining/backend/entities/Formation.java
+++ IT-Training/src/main/java/com/itTraining/backend/entities/Formation.java
@@ -1,5 +1,105 @@
 package com.itTraining.backend.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "formations")
 public class Formation {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "reference")
+	private String reference;
+	
+	@Column(name = "titre")
+	private String titre;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "prerequis")
+	private String prerequis;
+	
+	@Column(name = "public")
+	private String audience;
+	
+	@Column(name = "contenu")
+	private String contenu;
+	
+	@Column(name = "lien_test")
+	private String lienTest;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPrerequis() {
+		return prerequis;
+	}
+
+	public void setPrerequis(String prerequis) {
+		this.prerequis = prerequis;
+	}
+
+	public String getAudience() {
+		return audience;
+	}
+
+	public void setAudience(String audience) {
+		this.audience = audience;
+	}
+
+	public String getContenu() {
+		return contenu;
+	}
+
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+
+	public String getLienTest() {
+		return lienTest;
+	}
+
+	public void setLienTest(String lienTest) {
+		this.lienTest = lienTest;
+	}
+	
+	
+	
 }
--- IT-Training/src/main/java/com/itTraining/backend/repositories/FormationRepository.java
+++ IT-Training/src/main/java/com/itTraining/backend/repositories/FormationRepository.java
@@ -1,5 +1,19 @@
 package com.itTraining.backend.repositories;
 
-public interface FormationRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.itTraining.backend.entities.Formation;
+
+public interface FormationRepository extends JpaRepository<Formation, Long>{
+	
+	public List<Formation> findByReference(String reference);
+	public List<Formation> findByTitre(String titre);
+	public List<Formation> findByDescription(String description);
+	public List<Formation> findByPrerequis(String prerequis);
+	public List<Formation> findByAudience(String audience);
+	public List<Formation> findByContenu(String contenu);
+	public List<Formation> findByLienTest(String lienTest);
 
 }
