c91cb1dfbe5416d1c4032b63618bf3226c58d1cf
Pierre Krepa <pierre.krepa@hotmail.com>
2021-02-03 14:31:01 +0100

    Initialisation du projet Spring

---
+++ IT-Training/src/main/java/com/itTraining/backend/ItTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.itTraining.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItTrainingApplication.class, args);
+	}
+
+}
