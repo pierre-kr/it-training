c9e2d23b784d74971dacb5275cb91eb224867037
Quentin <quentinhorgnies@gmail.com>
2021-02-04 14:48:23 +0100

    delete lieu session

--- IT-Training/src/main/java/com/itTraining/backend/services/ParticipeService.java
+++ IT-Training/src/main/java/com/itTraining/backend/services/ParticipeService.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.itTraining.backend.entities.Participe;
 import com.itTraining.backend.repositories.ParticipeRepository;
 
+@Service
 public class ParticipeService {
 
 	@Autowired
